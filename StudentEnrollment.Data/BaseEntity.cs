2c835a6a75d8f9d6d94808990b56890430e32079
divacs <sonja.divac@yahoo.com>
2023-06-05 22:55:11 +0200

    tables for database

---
+++ StudentEnrollment.Data/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentEnrollment.Data
+{
+    public abstract class BaseEntity
+    {
+        public int id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ StudentEnrollment.Data/StudentEnrollmentDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Data
+{
+    public class StudentEnrollmentDBContext : IdentityDbContext
+    {
+        public StudentEnrollmentDBContext(DbContextOptions<StudentEnrollmentDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }   
+        public DbSet<Enrollment> Enrollments { get; set; } 
+    }
+}
