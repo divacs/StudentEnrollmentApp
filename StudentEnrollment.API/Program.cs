099fa56dca1150c284796b2c57d5bc78dc6219fb
divacs <sonja.divac@yahoo.com>
2023-07-01 11:56:33 +0200

    deleted endpoints from program.cs

--- StudentEnrollment.API/Program.cs
+++ StudentEnrollment.API/Program.cs
@@ -35,52 +35,6 @@ app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
 
-app.MapGet("/courses", async ([FromServices] StudentEnrollmentDbContext context) => 
-{
-    return await context.Courses.ToListAsync();
-    // na ovom endpointu dolazimo do lite kurseva iz baze
-
-}); // kako dolazim do ovog endpointa
-
-app.MapGet("courses/id", async ([FromServices] StudentEnrollmentDbContext context, int id) => // zelimo da dohvatimo podatak po idju
-{
-    return await context.Courses.FindAsync(id) is Course course ? Results.Ok(course) : Results.NotFound();
-
-});
-// metoda za dodavanje novog podatka u bazu
-app.MapPost("courses/", async (StudentEnrollmentDbContext context, Course course) => 
-{
-    await context.AddAsync(course);
-    await context.SaveChangesAsync();
-
-    return Results.Created($"/courses/{course.Id}", course);
-
-});
-// metoda za menjanje vec postojecih podataka, po idju
-app.MapPut("courses/{id}", async (StudentEnrollmentDbContext context, Course course, int id) =>
-{
-    var recordExists = await context.Courses.AnyAsync(q => q.Id == course.Id);
-    if (!recordExists) return Results.NotFound();
-
-    context.Update(course);
-    await context.SaveChangesAsync();
-
-    return Results.Created($"/courses/{course.Id}", course);
-
-});
-// brisanje podatka po idju
-app.MapDelete("courses/{id}", async (StudentEnrollmentDbContext context, Course course, int id) =>
-{
-    var record = await context.Courses.FindAsync(id);
-    if (record == null) return Results.NotFound();
-
-    context.Remove(record);
-    await context.SaveChangesAsync();
-    return Results.NoContent();
-
-
-});
-
 app.MapStudentEndpoints();
 
 app.MapEnrollmentEndpoints();
