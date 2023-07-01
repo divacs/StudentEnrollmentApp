8f724b7f181d0319793b45e798c1de2a11746b13
divacs <sonja.divac@yahoo.com>
2023-07-01 11:50:05 +0200

    ajusments

--- StudentEnrollment.Data/StudentEnrollmentDBContext.cs
+++ StudentEnrollment.Data/StudentEnrollmentDBContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using StudentEnrollment.Data.Configuration;
 using System;
 using System.Collections.Generic;
@@ -9,14 +11,13 @@ using System.Threading.Tasks;
 
 namespace StudentEnrollment.Data
 {
-    public class StudentEnrollmentDBContext : IdentityDbContext
+    public class StudentEnrollmentDbContext : IdentityDbContext
     {
-        public StudentEnrollmentDBContext(DbContextOptions<StudentEnrollmentDBContext> options) : base(options)
+        public StudentEnrollmentDbContext(DbContextOptions<StudentEnrollmentDbContext> options) : base(options)
         {
 
         }
 
-        //seeding the data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -25,7 +26,29 @@ namespace StudentEnrollment.Data
         }
 
         public DbSet<Course> Courses { get; set; }
-        public DbSet<Student> Students { get; set; }   
-        public DbSet<Enrollment> Enrollments { get; set; } 
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
     }
+
+    public class StudentEnrollmentDbContextFactory : IDesignTimeDbContextFactory<StudentEnrollmentDbContext>
+    {
+        public StudentEnrollmentDbContext CreateDbContext(string[] args)
+        {
+            // Get environment
+            //string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            // Build config
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            // Get connection string
+            var optionsBuilder = new DbContextOptionsBuilder<StudentEnrollmentDbContext>();
+            var connectionString = config.GetConnectionString("StudentEnrollmentDbConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+            return new StudentEnrollmentDbContext(optionsBuilder.Options);
+        }
+    }
+
 }
